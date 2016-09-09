de6a19fdf7ea8e0c5d0eea5178966fead6dc4a46
Valentin Hristov <valentin.hristov@gmail.com>
2016-09-09 21:46:45 +0300

    implement total numeric value

--- lesson4/app.js
+++ lesson4/app.js
@@ -1,8 +1,23 @@
 (function () {
-    'use strict';
+    'use strict'
     angular.module('myFirstApp', [])
     .controller('MyFirstController', function($scope) {
-      $scope.name = "Valyo";
+      $scope.name = ""
+      $scope.totalValue = 0
+
+      $scope.displayNumeric = function () {
+        var totalNameValue = calculateNumericForString($scope.name)
+        $scope.totalValue = totalNameValue
+      }
+
+      function calculateNumericForString(string) {
+          console.log(string)
+          var totalStringValue = 0
+          for(var i = 0; i < string.length; i++) {
+            totalStringValue += string.charCodeAt(i)            
+          }
+          return totalStringValue
+      }
     })
   }
 )();
