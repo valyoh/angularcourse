f90df57666c44e6ff46d1b60e7510a550564ff64
Valentin Hristov <valentin.hristov@gmail.com>
2016-09-12 09:01:36 +0300

    assignment 1

---
+++ module1-solution/app.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict'
+    angular.module('LunchChecker', [])
+    .controller('LunchCheckerController', LunchCheckerController);
+    LunchCheckerController.$inject = ['$scope'];
+    function LunchCheckerController($scope) {
+      $scope.dishes = "";
+      $scope.message = "";
+
+      $scope.checkIfTooMuch = function() {
+        var allDishes = $scope.dishes
+        var trimedDishes = allDishes.trim();
+
+        if(trimedDishes.length == 0) {
+          $scope.message = "Please enter data first"
+        } else {
+          var arrayOfStrings = trimedDishes.split(",");
+          var dishesCount = arrayOfStrings.length;
+          if(dishesCount > 0 && dishesCount < 4) {
+            $scope.message = "Enjoy!";
+          }
+        }
+      }
+
+    }
+  }
+)();
