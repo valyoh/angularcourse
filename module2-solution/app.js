b0a8b12cefc2ea522d72fcd6cc654deebde96400
Valentin Hristov <valentin.hristov@gmail.com>
2016-09-22 00:10:20 +0300

    Module 2 assignemnt

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -9,27 +9,60 @@
     function ToBuyShoppingController(ShoppingListCheckOffService) {
       var toBuyController = this;
       toBuyController.items = ShoppingListCheckOffService.getToBuyItems();
-      console.log(toBuyController.items);
+
+      toBuyController.isEmpty = ShoppingListCheckOffService.isEmptyToBuy();
+      toBuyController.notEmpty = ShoppingListCheckOffService.isAlradyBoughtNotEmpty();
+
+      toBuyController.markItemAsBought = function (itemIndex) {
+        ShoppingListCheckOffService.updateArrays(itemIndex);
+        toBuyController.isEmpty = ShoppingListCheckOffService.isEmptyToBuy();
+        toBuyController.notEmpty = ShoppingListCheckOffService.isAlradyBoughtNotEmpty();
+      }
     }
 
     AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
     function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
       var alreadyBoughtController = this;
+      alreadyBoughtController.items = ShoppingListCheckOffService.getAlreadyBoughtItems();
+      alreadyBoughtController.notEmpty = ShoppingListCheckOffService.isAlradyBoughtNotEmpty();
     }
 
     function ShoppingListCheckOffService() {
       var service = this;
+      var alreadyBoughtItems = [];
 
       var toBuyItems = [
         // "1","2","3"];
-        { name: "Beer", quantity: "200 bottles" },
-        { name: "Vodka", quantity: "90 bottles" },
-        { name: "Wine", quantity: "250 bottles" },
+        { name: "Beer", quantity: 200 },
+        { name: "Vodka", quantity: 90 },
+        { name: "Wine", quantity: 250 },
+        { name: "Cookies", quantity: 10 },
+        { name: "Orange Juice", quantity: 100 }
       ];
+
+      service.updateArrays = function(itemIndex) {
+        var item = toBuyItems[itemIndex];
+
+        alreadyBoughtItems.push(item);
+        toBuyItems.splice(itemIndex, 1);
+
+      };
+
+      service.getAlreadyBoughtItems = function () {
+          return alreadyBoughtItems;
+      };
 
       service.getToBuyItems = function () {
         return toBuyItems;
       };
+
+      service.isEmptyToBuy = function () {
+        return (toBuyItems.length == 0);
+      }
+
+      service.isAlradyBoughtNotEmpty = function () {
+        return (alreadyBoughtItems.length > 0);
+      }
     }
 
 })();
